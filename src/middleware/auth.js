9817ea2bd2f8977f86cb4a560f437f66971cda1a
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-10 22:43:05 +0200

    updated auth middleware to accept DEV jwt token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,22 +4,29 @@
 const jwt = require('jsonwebtoken');
 const genError = require('../helpers/error');
 
-const auth = (req, res, next) => {
-    if (!req.header('x-access-token')) {
+module.exports = auth = (req, res, next) => {
+    if (
+        process.env.NODE_ENV === 'development' &&
+        req.header('x-access-token') === process.env.DEVTOKEN
+    ) {
+        next();
+    } else if (!req.header('x-access-token')) {
         res.status(403).json(
             genError(403, 'No token provided', 'No access token provided in header'),
         );
     } else {
-        jwt.verify(req.header('x-access-token'), process.env.JWTSECRET, (err, decoded) => {
-            if (err) {
-                res.status(401).json(
-                    genError(401, 'Invalid token', 'Token invalid or has expired'),
-                );
-            } else {
-                next();
-            }
-        });
+        jwt.verify(
+            req.header('x-access-token'),
+            process.env.JWTSECRET || 'devmode',
+            (err, decoded) => {
+                if (err) {
+                    res.status(401).json(
+                        genError(401, 'Invalid token', 'Token invalid or has expired'),
+                    );
+                } else {
+                    next();
+                }
+            },
+        );
     }
 };
-
-module.exports = auth;
