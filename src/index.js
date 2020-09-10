a69c475a4444eb5140e076786881c11569edb38b
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-10 22:46:49 +0200

    forgot to commit base index.js file earlier

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/**
+ * Index file for running the express API server
+ */
+const express = require('express');
+require('dotenv').config();
+
+const server = express();
+server.use(express.json());
+server.use(express.json({ type: 'application/merge-patch+json' }));
+
+//Routers
+const login = require('./routers/login');
+const register = require('./routers/register');
+const reports = require('./routers/reports');
+const validate = require('./routers/validate');
+
+const port = process.env.PORT || 8080;
+
+//Mounting Routes
+server.use('/auth', login);
+server.use('/auth', register);
+server.use('/reports', reports);
+server.use('/validate', validate);
+
+server.get('/', auth, (req, res) => {
+    res.json({ BestPony: 'Littlepip' });
+});
+
+server.listen(port, console.log(`Listening on port ${port}`));
