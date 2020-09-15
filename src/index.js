40b7ee3f634b119743c2c79a4a8f36b796f2fe4c
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-15 02:04:08 +0200

    updated server to support jwt httponly cookes and csrf validation

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const csurf = require('csurf');
+
+const csrfProtection = csurf({
+    cookie: true,
+});
 
 const server = express();
 server.use(express.json());
 server.use(express.json({ type: 'application/merge-patch+json' }));
 server.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+server.use(cookieParser());
+server.use(csrfProtection);
 //Routers
-const login = require('./routers/login');
+const auth = require('./routers/auth');
 const register = require('./routers/register');
 const reports = require('./routers/reports');
 const validate = require('./routers/validate');
@@ -18,10 +26,10 @@ const validate = require('./routers/validate');
 const port = process.env.PORT || 8080;
 
 //Mounting Routes
-server.use('/auth', login);
-server.use('/auth', register);
-server.use('/', reports);
-server.use('/validate', validate);
+server.use('/api/auth', auth);
+server.use('/api/auth', register);
+server.use('/api/', reports);
+server.use('/api/validate', validate);
 
 server.get('/', auth, (req, res) => {
     res.json({ BestPony: 'Littlepip' });
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,28 +5,21 @@ const jwt = require('jsonwebtoken');
 const genError = require('../helpers/error');
 
 module.exports = auth = (req, res, next) => {
-    if (
-        process.env.NODE_ENV === 'development' &&
-        req.header('x-access-token') === process.env.DEVTOKEN
-    ) {
+    if (process.env.NODE_ENV === 'development' && req.cookies.token === process.env.DEVTOKEN) {
         next();
-    } else if (!req.header('x-access-token')) {
+    } else if (!req.cookies.token) {
         res.status(403).json(
             genError(403, 'No token provided', 'No access token provided in header'),
         );
     } else {
-        jwt.verify(
-            req.header('x-access-token'),
-            process.env.JWTSECRET || 'devmode',
-            (err, decoded) => {
-                if (err) {
-                    res.status(401).json(
-                        genError(401, 'Invalid token', 'Token invalid or has expired'),
-                    );
-                } else {
-                    next();
-                }
-            },
-        );
+        jwt.verify(req.cookies.token, process.env.JWTSECRET || 'devmode', (err, decoded) => {
+            if (err) {
+                res.status(401).json(
+                    genError(401, 'Invalid token', 'Token invalid or has expired'),
+                );
+            } else {
+                next();
+            }
+        });
     }
 };
