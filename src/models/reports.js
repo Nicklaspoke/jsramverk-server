30eaac4b029dceaa6acbbd156f34873578c0fafb
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-15 02:05:53 +0200

    added support for creating and editing reports

--- src/models/reports.js
+++ src/models/reports.js
@@ -7,6 +7,34 @@ const getReport = async (week) => {
     return res;
 };
 
+const createReport = async (content) => {
+    try {
+        await db('report').insert({
+            week: content.week,
+            title: content.title,
+            content: content.content,
+        });
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+const updateReport = async (content) => {
+    try {
+        await db('report')
+            .update({
+                title: content.title,
+                content: content.content,
+            })
+            .where({ week: content.week });
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
 module.exports = {
     getReport: getReport,
+    createReport: createReport,
+    updateReport: updateReport,
 };
--- src/routers/reports.js
+++ src/routers/reports.js
@@ -7,13 +7,31 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const validate = require('../middleware/validation');
 const genError = require('../helpers/error');
-const { getReport } = require('../models/reports');
-router.post('/reports', auth, validate, (req, res) => {
-    res.json({ LightBringer: 'Littlepip' });
+const { getReport, createReport, updateReport } = require('../models/reports');
+
+router.post('/reports', auth, validate, async (req, res) => {
+    const success = await createReport(req.body);
+    res.status(success ? 201 : 400).json(
+        success
+            ? { data: 'Report Created' }
+            : genError(
+                  400,
+                  'Invalid Week',
+                  'specified week is not valiid or has already a report created',
+              ),
+    );
+});
+
+router.put('/reports/week/:weekNum', auth, validate, async (req, res) => {
+    const sucess = await updateReport(req.body);
+    res.status(sucess ? 201 : 500).json(
+        sucess
+            ? { data: 'report updated' }
+            : genError(500, 'Internal server error', 'something went wrong when trying to update'),
+    );
 });
 
 router.get('/reports/week/:weekNum', async (req, res) => {
-    console.log('derp');
     const report = await getReport(req.params.weekNum);
 
     res.status(report.length !== 0 ? 200 : 404).json(
@@ -26,4 +44,5 @@ router.get('/reports/week/:weekNum', async (req, res) => {
               ),
     );
 });
+
 module.exports = router;
