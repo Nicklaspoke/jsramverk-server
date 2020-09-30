01537c034f2d7ac5fa2385999eb6b4c7872eff5e
Nicklaspoke <Nicklas.poke@gmail.com>
2020-09-30 02:25:15 +0200

    fixed csrf token, so its valid on all domains for now

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const genError = require('./helpers/error');
 const preFlightCheck = require('./helpers/preFlight');
 const csrfProtection = csurf({
     cookie: true,
-    domain: '*',
+    domain: '*.nicklaspoke.se',
 });
 
 const server = express();
